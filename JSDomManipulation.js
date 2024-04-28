1c3e3b54a9ec4fa9a62bba51077f0cc878f718e9
Yogendra Dawar <yogendradawar6@gmail.com>
2024-04-28 17:58:14 +0530

    Dom method

---
+++ JSDomManipulation.js
@@ -0,0 +1,48 @@
+// #13
+
+// JS DOM manipulation -
+
+    // window object -> global object 
+
+    // DOM(document object model)
+    // BOM(browser object model )
+
+
+    //Access the Element
+
+    // document.getElementById(id_name);
+    // document.getElementsByClassName(class_name);
+    // document.getElementsByTagName(by_tag);
+
+    // document.querySelector(name of class ,id , or tag only first Element will be return)
+    // document.querySelectorAll(all the tags , classe , id will be return )
+
+    // $0 use in console to how select the element and in console run $0 code
+
+    //Update Exiting Element
+
+        // .innerHTML()  --> get/set
+        // .outerHTML()
+        // .textContent() --> it will show all the code how we have written the code in code editor 
+        // .innerText() --- > yeh output vesa hi show krta hai jesa website ya ui pe show ho rha 
+
+
+    // Add elements
+
+        // creation ---> document.createElement('name');
+        // .textContent ---> add the text;
+        // .appendChild(name); ----> add the element always the child will add at last
+            // but i want to add element add with my position so for that the method is 
+                // insertAdjacentElement('position',elementname);   position -->  beforebeign,afterbegin,beforeend,afterend
+
+    // Removal
+
+        // removeChild(nameofchild);
+
+
+
+
+
+
+
+
